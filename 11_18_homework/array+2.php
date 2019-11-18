0c0e3ed74fe0a231f2a6ea01ba69ce6ff7f243b6
Aleksej100 <aleksej0126@gmail.com>
2019-11-18 18:29:40 +0200

    Update array+2.php

--- 11_18_homework/array+2.php
+++ 11_18_homework/array+2.php
@@ -1,16 +1,11 @@
 <?php
-//wrong
-//working on even
+
 $arr = [1,2,3,4,5,6,7,8,9,10];
 $length = count($arr);
 
-for($i=0; $i < $length; $i++) 
+for($i=0; $i < $length; $i+= 2) 
 { 
     # code...
-    if($arr[$i] % 2 == 0)
-    {
         echo $arr[$i];
         echo "\n";
-    }
-
 }
